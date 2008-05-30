57fdebcca6f2c963456b2e849761b546c69dd994
ramiro <ramiro@66570020-b32e-0410-962d-e44e8f9afd43>
2008-05-30 16:12:43 +0000

    Use correct quotes for including files.

--- mlp/mlpdec.c
+++ mlp/mlpdec.c
@@ -25,9 +25,9 @@
  */
 
 #include "avcodec.h"
-#include <libavutil/intreadwrite.h>
+#include "libavutil/intreadwrite.h"
 #include "bitstream.h"
-#include <libavutil/crc.h>
+#include "libavutil/crc.h"
 #include "parser.h"
 #include "mlp_parser.h"
 
