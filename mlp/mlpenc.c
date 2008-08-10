2fdbd9cc696e6ae541607bbe0eefcd3da5be11d9
ramiro <ramiro@66570020-b32e-0410-962d-e44e8f9afd43>
2008-08-10 13:38:30 +0000

    Only check for best bitcount if it was smaller than previous bitcount.

--- mlp/mlpenc.c
+++ mlp/mlpenc.c
@@ -812,10 +812,10 @@ static void codebook_bits(MLPEncodeContext *ctx, unsigned int substr,
                                  min, max, offset,
                                  &temp_bo, &next, !direction);
 
+            if (temp_bo.bitcount < previous_count) {
             if (temp_bo.bitcount < best_bo.bitcount)
                 best_bo = temp_bo;
 
-            if (temp_bo.bitcount < previous_count) {
                 is_greater = 0;
             } else if (++is_greater >= 5)
                 break;
